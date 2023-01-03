571c25924d4562ca52956a38aaedee61f7948650
Jonathan Vervaeke <vervaeke.jonathan@gmail.com>
2023-01-03 11:31:41 +0100

    AOC 2022 DAY 1

---
+++ d01_02/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let content = include_str!("input.txt").split("\n\n");
+    let mut score : Vec<i32> = content.map(|x| decode(x)).collect();
+    score.sort();
+    score.reverse();
+    println!("{:?}", score[0] + score[1] + score[2]);
+}
+
+fn decode(text : &str) -> i32 {
+    let summed = text.split("\n").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    summed.into_iter().sum()
+}
+
