1f1c8167dd2fcb8684b09e6c1910442e1458a144
Jonathan Vervaeke <vervaeke.jonathan@gmail.com>
2023-01-04 20:59:34 +0100

    AOC 2022 DAY 5

--- d04_02/src/main.rs
+++ d04_02/src/main.rs
@@ -7,13 +7,13 @@ fn main() {
 fn decode(text : &str) -> i32 {
     //println!("{}", text);
     let p = text.split(",")
-                                        .filter_map(|x| 
-                                                x.split_once("-")
-                                                  .and_then(|(l, r)| 
-                                                        Some((l.parse::<i32>().ok().unwrap(), 
-                                                              r.parse::<i32>().ok().unwrap())) )
-                                        )
-                                        .collect::<Vec<_>>();
+                .filter_map(|x| 
+                        x.split_once("-")
+                            .and_then(|(l, r)| 
+                                Some((l.parse::<i32>().ok().unwrap(), 
+                                        r.parse::<i32>().ok().unwrap())) )
+                )
+                .collect::<Vec<_>>();
     //println!("{:?}", p);
     let (part1_min,part1_max) = (p[0].0, p[0].1);
     let (part2_min,part2_max) = (p[1].0, p[1].1);
---
+++ d05_02/src/main.rs
@@ -0,0 +1,56 @@
+use std::collections::VecDeque;
+
+fn main() {
+    let (start_position, commands) = include_str!("input.txt").split_once("\n\n").unwrap();
+    let mut lines: Vec<_> = start_position.split("\n").map(|line| { line }).collect();
+    lines.reverse();
+    let mut stack : Vec<VecDeque<String>> = Vec::new();
+    for _ in 0 .. 9 {        
+        stack.push(VecDeque::<String>::new());
+    }
+
+    for line in &lines {
+        for i in 0 .. 9 {
+            decode_stack(&mut stack, i, line);
+        }
+    }
+
+    for line in commands.split("\n") {
+        //println!("{}", line);
+        let line = line.replace("move ", "");
+        let (total, (src, dst)) = line.split_once(" from ")
+                                 .map(|(l,r)| 
+                                        ( l.parse::<usize>().unwrap(), 
+                                          r.split_once(" to ")
+                                           .map(|(l,r)| 
+                                                ( l.parse::<usize>().unwrap() - 1,
+                                                  r.parse::<usize>().unwrap() - 1)
+                                                ).unwrap()
+                                        )
+                                    )
+                                 .unwrap();
+        //println!("{} {} {}", total, src, dst);
+
+        let start_point = stack[src].len() - total;
+
+        for i in start_point..stack[src].len() {
+            let v = stack[src].get(i).unwrap().to_string();
+            stack[dst].push_back(v);
+        }
+        
+        for _ in 0..total {
+            stack[src].pop_back().unwrap();
+        }     
+    }  
+
+    stack.iter().for_each(|s| print!("{}", s.back().unwrap() ) );
+    println!();
+
+}
+
+fn decode_stack(stack : &mut Vec<VecDeque<String>>, i : usize, line : &str) {
+    let letter = line.chars().nth((i * 4) + 1).unwrap();
+    if letter.is_alphabetic() {
+        stack[i].push_back(letter.to_string());
+    }
+}
