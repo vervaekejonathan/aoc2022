b7f64b14ad753ac8306787454ff23e715444c8be
Jonathan Vervaeke <vervaeke.jonathan@gmail.com>
2023-01-06 13:44:30 +0100

    AOC 2022 DAY 10 - improvements

--- d10_02/src/main.rs
+++ d10_02/src/main.rs
@@ -1,23 +1,30 @@
 
 fn main() {
-    let (mut value, mut clock_cycle) = (1, 1);
+    let mut value : i32 = 1;
+    let mut clock_cycle : i32 = 1;
     let mut image : Vec<char> = Vec::with_capacity(40*6);
-    let mut sprite : Vec<i32> = Vec::with_capacity(40*6);
-
-    for _ in 0..40*6 {
-        image.push('.');
-        sprite.push(0);
-    }
 
     include_str!("input.txt")
         .split("\n")
         .for_each(|line| 
         {   
-            sprite[clock_cycle-1] = value;
+            if value >= (((clock_cycle - 1 ) % 40) - 1) as i32 &&
+               value <= (((clock_cycle - 1 ) % 40) + 1) as i32
+            {
+                image.push('#');
+            } else {
+                image.push('.');
+            }
             clock_cycle += 1; 
 
             if line.contains("addx") {  
-                sprite[clock_cycle-1] = value;
+                if value >= (((clock_cycle - 1 ) % 40) - 1) as i32 &&
+                   value <= (((clock_cycle - 1 ) % 40) + 1) as i32
+                {
+                    image.push('#');
+                } else {
+                    image.push('.');
+                }
                 clock_cycle += 1;        
                 let (_, operand) = line.split_once(" ")
                                         .map(
@@ -29,24 +36,6 @@ fn main() {
             }
         }  
     );
-
-    sprite.chunks(40).for_each(|line|
-        {
-            line.iter().for_each(
-                    |x| 
-                    print!("{:0>2} ", x));
-            println!(); 
-        });
-
-    for i in 0..6 {
-        for j in 0..40 {
-            let index = j+(40*i);
-            if sprite[index] >= (j as i32) - 1 &&
-               sprite[index] <= (j as i32) + 1 {
-                image[index] = '#';
-            }
-        }
-    }
 
     image.chunks(40).for_each(|line|
         {
